065d258571d378d1edce39eb0c2886b786e24bf0
shadow0fthenight <marcenkotimur83@gmail.com>
2025-05-08 17:28:52 +0200

    aksdopakdpoakdpa

--- Assets/Scripts/Menu/LoseMenu.cs
+++ Assets/Scripts/Menu/LoseMenu.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class LoseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject loseMenu;
+
     void Start()
     {
-        
+        loseMenu.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnRestartButtonClick()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void OnMenuButtonClick()
+    {
+        SceneManager.LoadScene("Menu");
     }
 }
