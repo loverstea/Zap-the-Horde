14a269b1b4a819cf0b0dbf2672b2969e01bca80c
shadow0fthenight <marcenkotimur83@gmail.com>
2025-05-09 17:25:41 +0200

    dasdsa

--- Assets/Scripts/ScriptableObject/ScriptableOBJ.cs
+++ Assets/Scripts/ScriptableObject/ScriptableOBJ.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
-using Microsoft.Unity.VisualStudio.Editor;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
